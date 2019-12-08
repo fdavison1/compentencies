ee696296d23079656d2c5559d4da420246ca442d
fdavison1 <fred.noreply@gmail.com>
2019-12-08 13:58:34 -0700

    no match page

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import Todo from './components/Todo'
 import Position from './components/Position'
 import MatchObject from './components/MatchObject'
+import NoMatch from './views/NoMatch'
 
 export default (
     <Switch>
         <Route exact path='/' component={Todo}/>
         <Route path='/position' component={Position}/>
         <Route path='/matchobject' component={MatchObject}/>
+        <Route component={NoMatch} />
     </Switch>
 )
