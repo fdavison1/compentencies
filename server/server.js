7b7efbabc9d3bc920627796a43af57579effef72
fdavison1 <fred.noreply@gmail.com>
2019-12-08 14:55:46 -0700

    static files

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { SERVER_PORT } = process.env
 //TOP-LEVEL MIDDLEWARE
 const app = express()
 app.use(express.json())
+app.use(express.static('public'))
 
 //LISTENING
 app.listen(SERVER_PORT, () => console.log(`PORT ${SERVER_PORT} is working`))
--- src/components/StaticFiles.jsx
+++ src/components/StaticFiles.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const StaticFiles = () => {
+    const A = styled.a`
+    font-size: 2rem`
     return (
         <div>
             <h1>Express - Static Files</h1>
+
+            <A href='http://localhost:4002/Cat.jpg' target='_blank' rel='nooperner noreferrer'>Cat</A>
+
         </div>
     )
 }
